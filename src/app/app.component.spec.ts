d3c1bccae89a53cd5415d95566be477588663ae4
Mo Abjal <mohdafzal330@gmail.com>
2022-08-03 12:52:08 +0530

    TODO App Commit - Soroco

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'soroco-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('soroco-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('soroco-app app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'soroco-app';
+
+  public todos = [{isCompleted:false,title: 'Sample TODO 1'}];
+  public todoInput: string = '';
+
+  public handleTodoAddClick(){
+    if(!this.todoInput){
+      alert('Enter something into todo input box');
+      return;
+    }
+    this.addTodo({isCompleted:false, title: this.todoInput});
+    this.todoInput = '';
+  }
+
+  public handleCheckBoxChange(e,todo){
+    if(todo.isCompleted===false){
+      todo.isCompleted = true;
+    } else {
+      todo.isCompleted = false;
+    }
+    console.log(e.target);
+  }
+
+  public handleDelete(todo){
+    let idx = this.todos.indexOf(todo);
+    this.todos.splice(idx,1);
+
+  }
+
+  public getTotalLeft(){
+    return this.todos.filter(t=>t.isCompleted===false).length;
+  }
+  public getTotalCompleted(){
+    return this.todos.filter(t=>t.isCompleted===true).length;
+  }
+
+  private addTodo(todo){
+    if(!todo){
+      return;
+    }
+    this.todos.push(todo);
+  }
+
+
 }
